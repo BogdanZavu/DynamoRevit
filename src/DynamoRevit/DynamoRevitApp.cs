d400b352b90f8398cdb38dd43f80613561bc9764
Bogdan Zavu <zavu_bogdan@yahoo.com>
2016-06-13 13:24:50 -0400

    Manage DynamoRevitModel startup , expose its state and load an additional component which can benefit from this

--- src/DynamoRevit/DynamoRevitApp.cs
+++ src/DynamoRevit/DynamoRevitApp.cs
@@ -110,6 +110,39 @@ namespace Dynamo.Applications
         private static EventHandlerProxy proxy;
         private AddInCommandBinding dynamoCommand;
 
+        private Result loadDependentComponents()
+        {
+            var dynamoRevitRootDirectory = Path.GetDirectoryName(Path.GetDirectoryName(assemblyName));
+            var versionNumber = ControlledApplication.VersionNumber;
+
+            var dynamoRevitAditionsPath = Path.Combine(dynamoRevitRootDirectory,
+                string.Format("Revit_{0}", versionNumber), "DynamoRevitAdditions.dll");
+
+            if (File.Exists(dynamoRevitAditionsPath))
+            {
+                try
+                {
+                    var dynamoRevitAditionsAss = Assembly.LoadFrom(dynamoRevitAditionsPath);
+                    if (dynamoRevitAditionsAss != null)
+                    {
+                        var dynamoRevitAditionsLoader = dynamoRevitAditionsAss.CreateInstance("DynamoRevitAdditions.LoadManager");
+                        if (dynamoRevitAditionsLoader != null)
+                        {
+                            dynamoRevitAditionsLoader.GetType().GetMethod("Initialize").Invoke(dynamoRevitAditionsLoader, null);
+                            return Result.Succeeded;
+                        }
+                    }
+                }
+                catch(Exception ex)
+                {
+                    return Result.Failed;
+                }
+
+            }
+
+            return Result.Failed;
+        }
+
         public Result OnStartup(UIControlledApplication application)
         {
             // Revit2015+ has disabled hardware acceleration for WPF to
@@ -143,6 +176,8 @@ namespace Dynamo.Applications
                 RevitServicesUpdater.Initialize(DynamoRevitApp.Updaters);
                 SubscribeDocumentChangedEvent();
 
+                loadDependentComponents();
+
                 return Result.Succeeded;
             }
             catch (Exception ex)
@@ -165,8 +200,8 @@ namespace Dynamo.Applications
                 JournalData = journalData,
                 Application = application
             };
-            var cmd = new DynamoRevit();
-            return cmd.ExecuteCommand(data);
+        
+            return DynamoRevit.ExecuteCommand(data);
         }
 
         void executed(object sender, ExecutedEventArgs e)
